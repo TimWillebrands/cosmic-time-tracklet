2f6b0b816dca5a2c21aedf6ad54e6eae1a42c323
Tim Willebrands <timwillebrands@florinco.nl>
2025-02-24 11:21:50 +0100

    wayland foundations stand?

--- src/idle_monitor.rs
+++ src/idle_monitor.rs
@@ -1,83 +1,49 @@
-use std::error::Error;
-use wayland_client::{
-    protocol::wl_seat::WlSeat, 
-    Display, GlobalManager, 
+/*
+ * This guy knows the things:
+ *  https://smithay.github.io/book/client/general/registry.html
+ */
+use cosmic::cctk::wayland_client::{
+    globals::{registry_queue_init, Global, GlobalListContents},
+    protocol::wl_registry,
+    Connection, Dispatch, QueueHandle,
 };
-use wayland_protocols::ext::idle_notify::v1::client::{
-    ext_idle_notifier_v1::ExtIdleNotifierV1,
+use cosmic::cctk::wayland_protocols::ext::idle_notify::v1::client::{
     ext_idle_notification_v1::{self, ExtIdleNotificationV1},
+    ext_idle_notifier_v1::ExtIdleNotifierV1,
 };
+//use wayland_protocols::ext::idle_notify::v1::client::ext_idle_notification_v1::Event;
+use std::error::Error;
 
 /// The IdleMonitor encapsulates the idle-notification object.
 pub struct IdleMonitor {
-    idle_notification: ExtIdleNotificationV1,
+    //idle_notification: ExtIdleNotificationV1, // Use Main<ExtIdleNotificationV1>
 }
 
 impl IdleMonitor {
     /// Creates a new IdleMonitor.
     ///
-    /// * `display` - A reference to the Wayland display.
-    /// * `qh` - A reference to a QueueHandle.
-    /// * `globals` - A reference to a GlobalManager that has already performed an initial roundtrip.
-    ///
     /// Returns an `IdleMonitor` that will notify when the user has been idle for 5 minutes.
-    pub fn new(
-        display: &Display,
-        globals: &GlobalManager,
-    ) -> Result<Self, Box<dyn Error>> {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
         // Connect to the Wayland display.
-        let display = Display::connect_to_env()?;
-        
-        // Create an event queue.
-        let mut event_queue = display.create_event_queue();
-        let qh = event_queue.handle();
+        let conn = Connection::connect_to_env()?;
+        let queue = conn.new_event_queue::<IdleMonitor>();
+        let qh = queue.handle();
+        let (globals, queue) = registry_queue_init::<IdleMonitor>(&conn).unwrap();
 
-        // Bind to the ext_idle_notify global. This returns an error if not available.
-        let idle_notifier = globals
-            .instantiate_exact::<ExtIdleNotifierV1>(1)
-            .map_err(|_| "ext_idle_notify_v1 is not available on this compositor.")?;
-        
-        // Retrieve a wl_seat; this example uses the first available seat.
-        let seat = globals.instantiate_exact::<WlSeat>(1)?;
-        
-        // Create an idle notification with a 5-minute timeout (300000 ms).
-        let idle_notification = idle_notifier.get_idle_notification(300000, &seat, qh, ());
-        
-        Ok(Self { idle_notification })
-    }
-
-    /// Runs a blocking dispatch loop for idle events.
-    ///
-    /// Call this method to process incoming idle events. Depending on your
-    /// application, you might want to integrate this dispatch into your main
-    /// event loop or run it on a separate thread.
-    pub fn run(&self, event_queue: &mut wayland_client::EventQueue) -> Result<(), Box<dyn Error>> {
-        loop {
-            event_queue.blocking_dispatch(&mut IdleHandler)?;
-        }
+        Ok(Self { })
     }
 }
 
-///// A simple dispatch handler for idle notifications.
-//struct IdleHandler;
-//
-//impl<D> Dispatch<ExtIdleNotificationV1, ()> for IdleHandler {
-//    fn event(
-//        _state: &mut D,
-//        _proxy: &ExtIdleNotificationV1,
-//        event: ext_idle_notification_v1::Event,
-//        _data: &(),
-//        _conn: &wayland_client::Connection,
-//        _qh: &QueueHandle<D>,
-//    ) {
-//        match event {
-//            ext_idle_notification_v1::Event::Idled => {
-//                println!("User has been idle for at least 5 minutes.");
-//            }
-//            ext_idle_notification_v1::Event::Resumed => {
-//                println!("User activity resumed.");
-//            }
-//            _ => {}
-//        }
-//    }
-//}
+impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for IdleMonitor {
+    fn event(
+        state: &mut IdleMonitor,
+        proxy: &wl_registry::WlRegistry,
+        event: wl_registry::Event,
+        // The `GlobalListContents` is a container with an up-to-date list of
+        // the currently existing globals
+        data: &GlobalListContents,
+        conn: &Connection,
+        qhandle: &QueueHandle<IdleMonitor>,
+    ) {
+    }
+}
